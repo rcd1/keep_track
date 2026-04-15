01bf7e5f033a1fb69bb0d15cf4840e03640d43da
rcd1 <ryandavis659@gmail.com>
2026-04-15 16:22:13 -0400

    starting the home screen, makes you wonder

--- app/src/main/java/com/example/keepingtrack/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/keepingtrack/ui/screens/HomeScreen.kt
@@ -1,8 +1,82 @@
 package com.example.keepingtrack.ui.screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.keepingtrack.ui.theme.AppTypography
+@Preview
 @Composable
 fun HomeScreen() {
+    Scaffold(
+        modifier = Modifier,
+        topBar = {HomeTopBar()},
+        bottomBar = {
+
+        }
+    ) {
+
+    }
+}
+
+@Composable
+fun HomeTopBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(0.07f)
+            .background(MaterialTheme.colorScheme.primary),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        IconButton(
+            onClick = {}
+        ) {
+            Icon(Icons.Default.Settings,
+                "Options",
+                tint = MaterialTheme.colorScheme.onPrimary)
+        }
+        Text("4/15/2026",
+            style = AppTypography.headlineMedium,
+            fontStyle = FontStyle.Italic,
+            color = MaterialTheme.colorScheme.onPrimary
+        )
+        IconButton(
+            onClick = {}
+        ) {
+            Icon(Icons.Default.Edit,
+                "Edit Counters",
+                tint = MaterialTheme.colorScheme.onPrimary)
+        }
+        IconButton(
+            onClick = {}
+        ) {
+            Icon(Icons.Default.DateRange,
+                "View Metrics",
+                tint = MaterialTheme.colorScheme.onPrimary)
+        }
+    }
 
 }
--- app/src/main/java/com/example/keepingtrack/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/keepingtrack/ui/screens/LoginScreen.kt
@@ -3,35 +3,134 @@ package com.example.keepingtrack.ui.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.keepingtrack.ui.theme.AppTypography
 
+@Preview
 @Composable
 fun LoginScreen() {
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 15.dp)
             .background(MaterialTheme.colorScheme.background),
-        topBar = {},
+        topBar = {
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.10f)
+                    .background(MaterialTheme.colorScheme.primary),
+                horizontalArrangement = Arrangement.SpaceAround,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = "Keeping Track",
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    style = AppTypography.displayMedium,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(vertical = 15.dp)
+                    )
+                Icon(Icons.Default.Favorite, null)
+            }
+        },
         bottomBar = {}
-    ) { innerPadding ->
+    ) { _ ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding)
                 .background(MaterialTheme.colorScheme.primaryContainer),
-            verticalArrangement = Arrangement.SpaceAround,
+            verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-
+            LoginFields()
         }
+    }
+}
+
+@Composable
+fun LoginFields() {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .fillMaxHeight(0.7f)
+                .background(MaterialTheme.colorScheme.primaryContainer),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(Modifier.fillMaxHeight(0.3f))
+            // Username TextField
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Username", style = MaterialTheme.typography.labelLarge) },
+                singleLine = true,
+                trailingIcon = {
+                    IconButton(
+                        onClick = {},
+                    ) {
+                        Icon(Icons.Default.Clear, "Clear Username")
+                    }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email, imeAction = ImeAction.Next),
+                modifier = Modifier,
+            )
+            Spacer(Modifier.height(20.dp))
+            // Password TextField
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text("Password", style = MaterialTheme.typography.labelLarge) },
+                singleLine = true,
+                trailingIcon = {
+                    IconButton(
+                        onClick = {},
+                    ) {
+                        Icon(Icons.Default.Clear, "Clear Username")
+                    }
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Done),
+                modifier = Modifier,
+            )
+            Spacer(Modifier.height(30.dp))
+            Button(
+                onClick = {/*Need to give something the username and password*/},
+                modifier = Modifier
+            ) {
+                Text("Login")
+            }
     }
 }
